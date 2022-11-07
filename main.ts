57e5c33b88fe85265a087bfaa3c020a4e28af2d5
tshisha <113388366+tshisha@users.noreply.github.com>
2022-11-07 14:26:41 -0800

    WORK IN ON IT

--- main.ts
+++ main.ts
@@ -1,1 +1,57 @@
-
+function drawGrid () {
+    gridSprites = []
+    currentY = 0
+    for (let row of grid) {
+        currentX = 0
+        for (let gridspace of row) {
+            if (gridspace == 1) {
+                gridSprite = sprites.create(img`
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    3 3 3 3 3 3 3 3 3 3 
+                    `, SpriteKind.Player)
+                gridSprite.setPosition(currentX, currentY)
+                gridSprites.push(gridSprite)
+            }
+            currentX += 10
+        }
+        currentY += 10
+    }
+}
+let gridSprite: Sprite = null
+let currentX = 0
+let currentY = 0
+let gridSprites: Sprite[] = []
+let grid: number[][] = []
+let rowTemplate = [
+0,
+1,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0,
+0
+]
+grid = []
+for (let index = 0; index < 12; index++) {
+    grid.push(rowTemplate)
+}
+game.onUpdate(function () {
+    drawGrid()
+})
